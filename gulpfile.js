f226c2b34e8ca24535518059c74d770a55cc9bc7
Yan Zhu <picturepan2@hotmail.com>
2017-11-20 19:38:36 +0800

    Initial Instagram filter 1977

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleancss = require('gulp-clean-css');
+var csscomb = require('gulp-csscomb');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('watch', function() {
+  gulp.watch('./src/*.scss', ['build']);
+});
+
+gulp.task('build', function() {
+  gulp.src('./src/*.scss')
+    .pipe(sass({outputStyle: 'compact', precision: 10})
+      .on('error', sass.logError)
+    )
+    .pipe(autoprefixer())
+    .pipe(csscomb())
+    .pipe(gulp.dest('./dist'))
+    .pipe(cleancss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['build']);
